a5cd2c5dbfc3bb0972908112e097d05296fc7a88
Albert Espiña <Albert.Espinya@gmail.com>
2020-05-05 00:25:54 +0200

    ShiftRegister
    Utilizable tanto para el master como para el slave y de tamaño especificable

--- src/i2c_shift_register.v
+++ src/i2c_shift_register.v
@@ -1,1 +1,18 @@
+module I2C_SHIFT_REGISTER #( parameter LENGTH)(Clk, Rst, Enable, Buffer, InputBit, OutputBit);
+	
+	input Clk;
+ 	input Rst; 
+	input Enable; 
+	input InputBit;
+	output reg [LENGTH - 1: 0]Buffer;
+	output reg OutputBit;
+always @(posedge Clk)
+begin
+    	if (!Rst) Buffer = 1'b0;
+  	else if (Enable) begin
+		OutputBit <= Buffer[LENGTH - 1: LENGTH - 2];
+		Buffer <= {Buffer[LENGTH - 1: LENGTH - 2], InputBit};
+	end
+end
 
+endmodule
